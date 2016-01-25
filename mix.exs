96788ddf94cb600311e5a00431b9f310a72f313f
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2016-01-25 15:03:08 -0600

    Fix compile problem due to missing postgrex dep

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BorderPatrol.Mixfile do
 
   def project do
     [app: :borderpatrol,
-     version: "0.0.7",
+     version: "0.0.8",
      elixir: "~> 1.0", deps: deps]
   end
 
@@ -36,6 +36,7 @@ defmodule BorderPatrol.Mixfile do
       {:urna, git: "https://github.com/meh/urna", ref: "cceb5ef10b"},
       {:cauldron, "~> 0.1.5"},
       {:ecto, "~> 1.1.0"},
+      {:postgrex, "~> 0.11.0"},
       {:cisco_snmp_ex, git: "https://github.com/jonnystorm/cisco-snmp-elixir"},
       {:tftp_ex, git: "https://github.com/jonnystorm/tftp-elixir"},
       {:acl_ex, git: "https://github.com/jonnystorm/acl-elixir"}
